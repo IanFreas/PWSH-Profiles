c127340ae162c4f62a351a0a35c1236b3837e3bd
Ian Freas <Ian.freas@gmail.com>
2025-04-12 17:50:43 -0700

    formatting to OTBS

--- linux_profile.ps1
+++ linux_profile.ps1
@@ -3,31 +3,31 @@ new-alias sel select-object
 new-alias l get-childitem
 
 function prompt {
-        write-host "[$((get-date).ToString('hh:mm'))]" -nonewline -foregroundcolor yellow
-	if ($env:USER -eq 'root') {	
-            write-host "($($env:USER)@$(hostname))" -foregroundcolor red
-	} else {
-            write-host "($($env:USER)@$(hostname))" -foregroundcolor green
-	}
-        write-host "$(get-location)" -foregroundcolor blue
-        write-host $(if ($nestedpromptlevel -ge 1) {'>>'}) -nonewline
-        return 'PS>'
+    write-host "[$((get-date).ToString('hh:mm'))]" -nonewline -foregroundcolor yellow
+    if ($env:USER -eq 'root') {	
+        write-host "($($env:USER)@$(hostname))" -foregroundcolor red
+    } else {
+        write-host "($($env:USER)@$(hostname))" -foregroundcolor green
+    }
+    write-host "$(get-location)" -foregroundcolor blue
+    write-host $(if ($nestedpromptlevel -ge 1) { '>>' }) -nonewline
+    return 'PS>'
 }
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 #TODO: Fix duplicate entries showing up, eg, twitch
 function Get-Password {
     [cmdletbinding()]
     param(
-    	[parameter(position=0,valuefrompipeline)]
+        [parameter(position = 0, valuefrompipeline)]
         [string] $PasswordName
     )
 
-    if ($PSBoundParameters.ContainsKey('PasswordName')){
+    if ($PSBoundParameters.ContainsKey('PasswordName')) {
         $passwordItem = op item get $PasswordName --reveal --format json | ConvertFrom-Json 
-        $passwordOutput = $passwordItem | Select-Object Name, @{n='Username';e={($_.fields).where({$_.id -eq 'username'}).value}}, @{n='Password';e={($_.fields).where({$_.id -eq 'password'}).value}}
+        $passwordOutput = $passwordItem | Select-Object Name, @{n = 'Username'; e = { ($_.fields).where({ $_.id -eq 'username' }).value } }, @{n = 'Password'; e = { ($_.fields).where({ $_.id -eq 'password' }).value } }
     } else {
         $passwordItem = op item list --vault Private --format json | ConvertFrom-Json
-	$passwordOutput = $passwordItem.Title
+        $passwordOutput = $passwordItem.Title
     }
 
     return $passwordOutput
